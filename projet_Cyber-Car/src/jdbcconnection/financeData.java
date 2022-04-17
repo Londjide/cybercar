66fd8b392c52f4ee23e1bf7f3231ba54d23a5583
GostMM <mroivili.moustoifa@gmail.com>
2022-04-17 20:12:57 +0400

    Update financeData.java

--- projet_Cyber-Car/src/jdbcconnection/financeData.java
+++ projet_Cyber-Car/src/jdbcconnection/financeData.java
@@ -1,9 +1,38 @@
 package jdbcconnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public class financeData
 {
 	
+
+	private double chiffredDaffaire;
+	PreparedStatement stmt;
+	Connection connect;
+	ResultSet resultat;
 	
 	
+	public void getChiffreDaffaire() throws SQLException
+	{
+		
+		connect = ConnectoDataBase.getConnection();
+		String query = "call chiffreDaffaire()";
+		stmt = connect.prepareStatement(query);
+		resultat = stmt.executeQuery();
+		while(resultat.next())
+		{
+			this.chiffredDaffaire = resultat.getDouble("chiffre");
+		}
+		
+
+	}
 
 }
+	
+	
+	
+	
